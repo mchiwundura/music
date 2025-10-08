b282974997e683e95825a7fd9de00dfc208b590e
mchiwundura <mchiwundura@protonmail.com>
2025-10-08 22:00:42 +0200

    fixed the timing of the intro drums in smells like teen spirit and also added more cycles to match the actual song, and reduced gain on that electric guitar, it was too loud

--- songs/smells_like_teen_spirit.ts
+++ songs/smells_like_teen_spirit.ts
@@ -1,10 +1,10 @@
-setcpm(112 / 8)
+setcpm(116 / 8)
 // I'm an idiot that's why I made this with 8 bars not 4 
 
 let mainriff = "[f3, c4, f4] _ _ [f3, c4, f4] _ _ _ _ [a#3, f4, a#4] _ [a#3, f4, a#4] _ _ [g#3, c#4, g#4] _ _ [g#3, c#4, g#4] _ _ _ [c#4, g#4, c#5] _ [c#4, g#4, c#5] _"
 let mainriffbass = "[f2, c3, f3] _ _ [f2, c3, f3] _ _ _ _ [a#2, f3, a#3] _ [a#2, f3, a#3] _ _ [g#2, c#3, g#3] _ _ [g#2, c#3, g#3] _ _ _ [c#3, g#3, c#4] _ [c#3, g#3, c#4] _"
 let lead = note(mainriff).s("gm_acoustic_guitar_nylon")
-let electric_lead = note(mainriff).s("gm_overdriven_guitar")
+let electric_lead = note(mainriff).s("gm_overdriven_guitar").gain(0.2)
 let bass = note(mainriffbass).s("gm_electric_bass_pick")
 
 // this drum loop actually came better than i expected
@@ -25,11 +25,11 @@ let electric_lead2 = note("~@0.2 [~@0.2 <f4>@2, <c4>]").s("gm_acoustic_guitar_ny
 let hi_hat3 = note("c8 _ _ c _ _ _ c _ _ _ c _ _ _ c _").s("hh")
 let snare3 = note("c _ _ _ c _ _ _ c _ _ _ c _ _ _").s("bossdr110_sd").gain(2)
 let kick3 = note("~ c c _ _ c c _ _ c c _ _ c c _").s("bd")
-
+let leadend = note("[g#3, c#4, g#4] _ _ [g#3, c#4, g#4] _ _ _ [c#4, g#4, c#5] _ [c#4, g#4, c#5] _").s("gm_acoustic_guitar_nylon")
 
 arrange(
-	[2, lead],
-	[0.5, stack(kick3, snare3, hi_hat3).cpm(112 / 4)],
-	[2, stack(lead, kick, crash, snare, kickdelay, bass, electric_lead)],
-	[2, stack(bass2, kick2, snare2, electric_lead2)]
+	[1.5, lead],
+	[0.5, stack(stack(kick3, snare3, hi_hat3).cpm(112 / 4), leadend.cpm(116/4))],
+	[4, stack(lead, kick, crash, snare, kickdelay, bass, electric_lead)],
+	[4, stack(bass2, kick2, snare2, electric_lead2)]
 )._pianoroll()
